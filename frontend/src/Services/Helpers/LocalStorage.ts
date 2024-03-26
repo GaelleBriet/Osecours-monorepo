27654dde43fe1a31ddbc7e4da5ca7f258f7f993e
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-26 23:04:11 +0100

    create localstorage functions

---
+++ frontend/src/Services/Helpers/LocalStorage.ts
@@ -0,0 +1,12 @@
+export function getFromStorage(key: string): unknown | null {
+	const value = localStorage.getItem(key);
+	return value !== 'undefined' && value !== null ? JSON.parse(value) : null;
+}
+
+export function setToStorage(key: string, value: unknown): void {
+	localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function removeFromStorage(key: string): void {
+	localStorage.removeItem(key);
+}
