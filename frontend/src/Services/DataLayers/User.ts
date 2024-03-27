b5a1e1a5709757d1c928d83e60de00eb748baf02
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-27 16:07:17 +0100

    install axios

---
+++ frontend/src/Services/DataLayers/User.ts
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+// axios.post('http://your-laravel-app-url/login', {
+//     email: 'user@example.com',
+//     password: 'userpassword',
+// })
+// .then(response => {
+//     console.log(response.data);
+// })
+// .catch(error => {
+//     console.error(error);
+// });
