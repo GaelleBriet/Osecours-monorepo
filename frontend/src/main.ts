f7850a81183e7c9a1bf8c06ab953518d6d59ee62
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-27 09:33:50 +0100

    reinstall tailwind css + config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,10 @@
-import '@/Assets/Scss/style.css';
+import '@/Assets/Css/style.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import i18n from '@/Services/Translations/index.ts';
-
+import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import router from '@/Router';
 
@@ -12,5 +12,8 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
+app.use(PrimeVue, {
+	unstyled: true,
+});
 
 app.mount('#app');
