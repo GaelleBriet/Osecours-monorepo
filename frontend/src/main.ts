b0422112efddc14582fe77f3e110d50f957dfd7b
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-28 10:00:17 +0100

    Formkit: import packages & config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,21 @@
+import '@formkit/themes/genesis';
+import '@formkit/pro/genesis';
 import '@/Assets/Css/style.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { plugin } from '@formkit/vue';
 
 import i18n from '@/Services/Translations/index.ts';
-import App from './App.vue';
 import router from '@/Router';
+import formkitConfig from '../formkit.config.js';
+import App from './App.vue';
 
 const app = createApp(App);
+
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
+app.use(plugin, formkitConfig);
 
 app.mount('#app');
