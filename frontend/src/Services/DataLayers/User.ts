4108b95f27e41b350dde2a9409814fb274f34374
GaelleBriet <gaelle.briet@protonmail.com>
2024-03-28 17:44:36 +0100

    route api/login ok

--- frontend/src/Services/DataLayers/User.ts
+++ frontend/src/Services/DataLayers/User.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import { User } from '@/Interfaces/User.ts';
 
-const API_URL: string = 'http://localhost/api/login';
+const API_URL: string = 'http://localhost:8000/api';
 
 export const login = async (email: string, password: string): Promise<User> => {
 	try {
 		if (!email || !password)
 			throw new Error('Email and password are required.');
-		const response = {
-			data: {
-				firstName: 'Alice',
-				lastName: 'Doe',
-				phone: '1234567890',
-				existingCatCount: 1,
-				existingChildrenCount: 2,
-				existingDogCount: 0,
-				email: '',
-			},
-		};
-		// const response = await axios.post(`${API_URL}/login`, { email, password });
+		// const response = {
+		// 	data: {
+		// 		firstName: 'Alice',
+		// 		lastName: 'Doe',
+		// 		phone: '1234567890',
+		// 		existingCatCount: 1,
+		// 		existingChildrenCount: 2,
+		// 		existingDogCount: 0,
+		// 		email: '',
+		// 	},
+		// };
+		const response = await axios.post(`${API_URL}/login`, { email, password });
 		return response.data;
 	} catch (error) {
 		console.error(error);
