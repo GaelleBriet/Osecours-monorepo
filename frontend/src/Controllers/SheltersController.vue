bd56174719584cdf6067d597882321c0e89645ab
GaelleBriet <gaelle.briet@protonmail.com>
2024-04-06 22:50:12 +0200

    fix

--- frontend/src/Controllers/SheltersController.vue
+++ frontend/src/Controllers/SheltersController.vue
@@ -1,20 +1,4 @@
-<script setup lang="ts">
-	import TabsComponent from '@/Components/TabsComponent.vue';
-	import { ref } from 'vue';
-
-	const currentTab = ref(0);
-
-	const updateCurrentTab = (index) => {
-		currentTab.value = index;
-	};
-</script>
+<script setup lang="ts"></script>
 <template>
-	<div>
-		<TabsComponent
-			:tabs="[{ name: 'un' }, { name: 'deux' }]"
-			@update:current-tab="updateCurrentTab"
-		/>
-		<div v-if="currentTab === 0">premier contenu</div>
-		<div v-if="currentTab === 1">second contenu</div>
-	</div>
+	<div></div>
 </template>
