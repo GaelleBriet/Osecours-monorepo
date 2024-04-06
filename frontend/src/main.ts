68bc154d7c056d166bb6b69ef18e708a391f3b69
GaelleBriet <gaelle.briet@protonmail.com>
2024-04-06 12:35:59 +0200

    added new package : floating-vue (for floating label / tooltips)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,12 @@
 import '@formkit/themes/genesis';
 import '@formkit/pro/genesis';
 import '@/Assets/Css/style.css';
+import 'floating-vue/dist/style.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { plugin } from '@formkit/vue';
-
+import FloatingVue from 'floating-vue';
 import i18n from '@/Services/Translations/index.ts';
 import router from '@/Router';
 import formkitConfig from '../formkit.config.js';
@@ -16,6 +17,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
+app.use(FloatingVue);
 app.use(plugin, formkitConfig);
 
 app.mount('#app');
