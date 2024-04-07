4cc6a5da789decb0b6c5fb8c5e88e6f09a83ad08
GaelleBriet <gaelle.briet@protonmail.com>
2024-04-07 16:11:29 +0200

    prepare : store / datalayer members/families

--- frontend/src/Controllers/Families/FamiliesController.vue
+++ frontend/src/Controllers/Families/FamiliesController.vue
@@ -1,5 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import DataGridComponent from '@/Components/DataGridComponent.vue';
+	import { useUserStore } from '@/Stores/UserStore.ts';
+	import { computed, onMounted } from 'vue';
+	import { getCapitalizedText } from '@/Services/Helpers/TextFormat.ts';
+	import i18n from '@/Services/Translations';
 
-<template></template>
+	const t = i18n.global.t;
+	const userStore = useUserStore();
+	const families = computed(() => userStore.users);
+
+	const editItem = () => {
+		console.log('edit');
+	};
+
+	onMounted(async () => {
+		await userStore.getUsers();
+	});
+</script>
+
+<template>
+	<div>
+		<DataGridComponent
+			:store="userStore"
+			:model-value="families"
+			:title="getCapitalizedText(t('navigation.families'))"
+			:description="getCapitalizedText(t('navigation.families'))"
+			:columns="[
+				{ label: 'Nom', key: 'name' },
+				{ label: 'Nombre d\'animaux', key: 'icad' },
+				{ label: 'Adresse', key: 'species', visibility: { sm: true } },
+			]"
+			@edit="editItem"
+		/>
+	</div>
+</template>
 
 <style scoped lang="postcss"></style>
---
+++ frontend/src/Services/DataLayers/Member.ts
@@ -0,0 +1,184 @@
+import axiosInstance from '@/Services/DataLayers/AxiosInstance.ts';
+import { User } from '@/Interfaces/User.ts';
+import { ErrorResponse } from '@/Interfaces/Requests.ts';
+import { AxiosError } from 'axios';
+import { errorResponse } from '@/Services/Requests/RequestsResponses.ts';
+
+export const getMemberById = async (
+	id: string,
+): Promise<Partial<User> | ErrorResponse> => {
+	try {
+		const response = {
+			id: 1,
+			firstName: 'John',
+			lastName: 'Doe',
+			email: 'john.doe@mail.fr',
+			phone: '0612345678',
+			existingCatCount: 0,
+			existingDogCount: 0,
+			existingChildrenCount: 0,
+			adoptFamily: false,
+			fosterFamily: true,
+		};
+		return response;
+		// const response: AxiosResponse<User> = await axiosInstance.get<User>(
+		// 	`${import.meta.env.VITE_USERS_API_URL}/${id}`,
+		// );
+		// return response.data;
+	} catch (error) {
+		const axiosError: AxiosError = error as AxiosError;
+		return errorResponse(axiosError);
+	}
+};
+
+export const getMembers = async (): Promise<User[] | ErrorResponse> => {
+	try {
+		const members: User[] = [
+			{
+				id: 1,
+				firstName: 'John',
+				lastName: 'Doe',
+				email: 'john.doe@mail.fr',
+				phone: '0612345678',
+				existingCatCount: 0,
+				existingDogCount: 0,
+				existingChildrenCount: 0,
+				adoptFamily: false,
+				fosterFamily: true,
+			},
+			{
+				id: 2,
+				firstName: 'Jane',
+				lastName: 'Doe',
+				email: 'jane.doa@mail.fr',
+				phone: '0612345678',
+				existingCatCount: 0,
+				existingDogCount: 1,
+				existingChildrenCount: 0,
+				adoptFamily: true,
+				fosterFamily: false,
+			},
+		];
+		return members;
+		// const response: AxiosResponse<User[]> = await axiosInstance.get<User[]>(
+		// 	`${import.meta.env.VITE_USERS_API_URL}`,
+		// );
+		// return response.data;
+	} catch (error) {
+		const axiosError: AxiosError = error as AxiosError;
+		return errorResponse(axiosError);
+	}
+};
+
+export const getMembersByFamilyType = async (
+	familyType: string,
+): Promise<User[] | ErrorResponse> => {
+	try {
+		const members: User[] = [
+			{
+				id: 1,
+				firstName: 'John',
+				lastName: 'Doe',
+				email: 'john.doe@mail.fr',
+				phone: '0612345678',
+				existingCatCount: 0,
+				existingDogCount: 0,
+				existingChildrenCount: 0,
+				adoptFamily: false,
+				fosterFamily: true,
+			},
+			{
+				id: 2,
+				firstName: 'Jane',
+				lastName: 'Doe',
+				email: 'jane.doa@mail.fr',
+				phone: '0612345678',
+				existingCatCount: 0,
+				existingDogCount: 1,
+				existingChildrenCount: 0,
+				adoptFamily: true,
+				fosterFamily: false,
+			},
+		];
+		return members.filter((member: User) => {
+			if (familyType === 'foster') {
+				return member.fosterFamily;
+			} else if (familyType === 'adopt') {
+				return member.adoptFamily;
+			}
+		});
+	} catch (error) {
+		const axiosError: AxiosError = error as AxiosError;
+		return errorResponse(axiosError);
+	}
+};
+
+export const createMember = async (
+	member: User,
+): Promise<User | ErrorResponse> => {
+	try {
+		const response: User = {
+			id: 3,
+			firstName: 'Jack',
+			lastName: 'Doe',
+			email: 'jack.doe@mail.fr',
+			phone: '0612345678',
+			existingCatCount: 0,
+			existingDogCount: 0,
+			existingChildrenCount: 0,
+			adoptFamily: false,
+			fosterFamily: true,
+		};
+		return response;
+		// const response: AxiosResponse<User> = await axiosInstance.post<User>(
+		// 	`${import.meta.env.VITE_USERS_API_URL}`,
+		// 	member,
+		// );
+		// return response.data;
+	} catch (error) {
+		const axiosError: AxiosError = error as AxiosError;
+		return errorResponse(axiosError);
+	}
+};
+
+export const updateMember = async (
+	member: User,
+): Promise<User | ErrorResponse> => {
+	try {
+		const response: User = {
+			id: 3,
+			firstName: 'Jack',
+			lastName: 'Doe',
+			email: 'jack.doe@mail.fr',
+			phone: '0612345678',
+			existingCatCount: 0,
+			existingDogCount: 0,
+			existingChildrenCount: 0,
+			adoptFamily: false,
+			fosterFamily: true,
+		};
+		return response;
+		// const response: AxiosResponse<User> = await axiosInstance.put<User>(
+		// 	`${import.meta.env.VITE_USERS_API_URL}/${member.id}`,
+		// 	member,
+		// );
+		// return response.data;
+	} catch (error) {
+		const axiosError: AxiosError = error as AxiosError;
+		return errorResponse(axiosError);
+	}
+};
+
+export const deleteMember = async (id: string): Promise<boolean> => {
+	try {
+		const response: boolean = true;
+		return response;
+		// const response: AxiosResponse<boolean> = await axiosInstance.delete<boolean>(
+		// 	`${import.meta.env.VITE_USERS_API_URL}/${id}`,
+		// );
+		// return response.data;
+	} catch (error) {
+		// const axiosError: AxiosError = error as AxiosError;
+		return false;
+	}
+};
