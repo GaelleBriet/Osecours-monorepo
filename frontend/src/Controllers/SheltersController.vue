8b2a01d874c017f6faedbdfc8a10e96e142ed582
uriostegui-sam <samyy.uv@gmail.com>
2024-04-14 20:05:06 +0200

    shelters datagrid working

--- frontend/src/Controllers/SheltersController.vue
+++ frontend/src/Controllers/SheltersController.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 	import TabsComponent from '@/Components/TabsComponent.vue';
 	import { ref } from 'vue';
 
@@ -17,4 +17,53 @@
 		<div v-if="currentTab === 0">premier contenu</div>
 		<div v-if="currentTab === 1">second contenu</div>
 	</div>
+</template> -->
+<script setup lang="ts">
+	import DataGridComponent from '@/Components/DataGridComponent.vue';
+	import { useSheltersStore } from '@/Stores/SheltersStore.ts';
+	import { computed, onMounted } from 'vue';
+	import { getCapitalizedText } from '@/Services/Helpers/TextFormat.ts';
+	import i18n from '@/Services/Translations';
+	import { useRouter } from 'vue-router';
+
+	const t = i18n.global.t;
+	const router = useRouter();
+	const sheltersStore = useSheltersStore();
+	//const shelters = computed(() => sheltersStore.shelters);
+
+	const sheltersTransformed = computed(() => {
+		return sheltersStore.shelters.map((shelter) => ({
+			...shelter
+		}));
+	});
+
+	const editItem = (item) => {
+		router.push({
+			name: 'EditShelter',
+			params: { id: item.id },
+		});
+	};
+
+	onMounted(async () => {
+		await sheltersStore.getShelters();
+	});
+</script>
+
+<template>
+	<div class="w-full p-0">
+		<DataGridComponent
+			:store="sheltersStore"
+			:model-value="sheltersTransformed"
+			:title="getCapitalizedText(t('navigation.shelters'))"
+			:description="getCapitalizedText(t('pages.shelters.title'))"
+			:columns="[
+				{ label: getCapitalizedText(t('common.name')), key: 'name' },
+				{ label: getCapitalizedText(t('pages.shelters.quantity')), key: 'animals' },
+				{ label: getCapitalizedText(t('pages.shelters.phone')), key: 'phone'},
+				]"
+			@edit="editItem"
+		/>
+	</div>
 </template>
+
+<style scoped></style>
