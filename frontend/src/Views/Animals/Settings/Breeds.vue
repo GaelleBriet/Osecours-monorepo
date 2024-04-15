3370b433f89844a57b8c805d32bd5c32fe8cb7b7
GaelleBriet <gaelle.briet@protonmail.com>
2024-04-15 10:08:39 +0200

    WIP: animalsSettings Breed

--- frontend/src/Views/Animals/Settings/Breeds.vue
+++ frontend/src/Views/Animals/Settings/Breeds.vue
@@ -1,4 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { useAnimalsSettingsStore } from '@/Stores/AnimalsSettingsStore.ts';
+	import { Breed } from '@/Interfaces/Breed.ts';
+	import { onMounted, ref } from 'vue';
+	import DataGridComponent from '@/Components/DataGridComponent.vue';
+
+	const animalSettingsStore = useAnimalsSettingsStore();
+	const breeds = ref<Breed[]>([]);
+
+	onMounted(async () => {
+		await animalSettingsStore.getAllBreeds();
+		breeds.value = animalSettingsStore.breeds;
+	});
+</script>
 <template>
-	<div>BREEDS</div>
+	<div class="w-full p-0 relative -top-[35px]">
+		<DataGridComponent
+			:store="animalSettingsStore"
+			:model-value="breeds"
+			:columns="[
+				{ label: 'Race', key: 'name' },
+				{ label: 'Description', key: 'description' },
+			]"
+			:animals-chars="true"
+		/>
+	</div>
 </template>
