6d5f6e995ffffb0001f229976b94ca793427be19
uriostegui-sam <samyy.uv@gmail.com>
2024-04-15 14:26:07 +0200

    WIP: view of admin for all associations

--- frontend/src/Controllers/AssociationController.vue
+++ frontend/src/Controllers/AssociationController.vue
@@ -19,9 +19,6 @@ const t = i18n.global.t;
 const router = useRouter();
 const associationsStore = useAssociationsStore();
 //const association = computed(() => associationsStore.association);
-const updateCurrentTab = (index) => {
-	currentTab.value = index;
-};
 
 const associationsTransformed = computed(() => {
 	return associationsStore.associations.map((association) => ({
@@ -48,48 +45,55 @@ onMounted(async () => {
 </script>
 
 <template>
-	<div class="container">
-		<div class="grid grid-cols-3 grid-rows-1 gap-4">
-			<div class="text-2xl mb-1 col-span-2">
-				{{ getCapitalizedText(t('pages.associations.card')) }}:
-				{{ currentAssociation?.name }}
-			</div>
-			<div class="col-start-3 text-center lg:text-right">
-
-				<span v-if="currentAssociation">
-					<span class="inline-block rounded-full px-3 py-1 text-sm font-semibold leading-tight" :class="{
-					'bg-gray-200 text-gray-800': currentAssociation.request_status === 1, // pending
-					'bg-blue-200 text-blue-800': currentAssociation.request_status === 2, // in_progress
-					'bg-red-200 text-red-800': currentAssociation.request_status === 3, // cancelled
-					'bg-yellow-200 text-yellow-800': currentAssociation.request_status === 4, // on_hold
-					'bg-green-200 text-green-800': currentAssociation.request_status === 5, // completed
-					'bg-purple-200 text-purple-800': currentAssociation.request_status === 6, // archived
-					'bg-orange-200 text-orange-800': currentAssociation.request_status === 7, // draft
-					'bg-indigo-200 text-indigo-800': currentAssociation.request_status === 8, // accepted
-					'bg-pink-200 text-pink-800': currentAssociation.request_status === 9, // rejected
-				}">
-						{{ getCapitalizedText(t('pages.animals.status')) }}: {{
-					generateOptionsFromEnum(AssociationsRequestStatus,
-						'enums.associationStatus')[currentAssociation.request_status - 1]?.label }}
+	<div class="">
+		<template v-if="userStore.user?.scopes !== 'global_access_scope'">
+			<div class="grid grid-cols-3 grid-rows-1 gap-4">
+				{{ getCapitalizedText(t('pages.associations.card')) }}: {{ currentAssociation?.name }}
+	
+				<div class="col-start-3 text-center lg:text-right">
+					<span v-if="currentAssociation">
+						<span class="inline-block rounded-full px-3 py-1 text-sm font-semibold leading-tight" :class="{
+							'bg-gray-200 text-gray-800': currentAssociation.request_status === 1, // pending
+							'bg-blue-200 text-blue-800': currentAssociation.request_status === 2, // in_progress
+							'bg-red-200 text-red-800': currentAssociation.request_status === 3, // cancelled
+							'bg-yellow-200 text-yellow-800': currentAssociation.request_status === 4, // on_hold
+							'bg-green-200 text-green-800': currentAssociation.request_status === 5, // completed
+							'bg-purple-200 text-purple-800': currentAssociation.request_status === 6, // archived
+							'bg-orange-200 text-orange-800': currentAssociation.request_status === 7, // draft
+							'bg-indigo-200 text-indigo-800': currentAssociation.request_status === 8, // accepted
+							'bg-pink-200 text-pink-800': currentAssociation.request_status === 9, // rejected
+						}">
+							{{ getCapitalizedText(t('pages.animals.status')) }}: {{
+							generateOptionsFromEnum(AssociationsRequestStatus,
+								'enums.associationStatus')[currentAssociation.request_status - 1]?.label
+							}}
+						</span>
 					</span>
-				</span>
+				</div>
 			</div>
+		</template>
+		<template v-else>
+		</template>
 
-		</div>
-		<TabsComponent id="associationsTabsComponent" :tabs="[
-					{ name: getCapitalizedText(t('pages.animals.details')) },
-					{ name: getCapitalizedText(t('navigation.all-associations')) },
-				]" @update:current-tab="updateCurrentTab" />
 		<div class="content">
 			<template v-if="currentTab === 0 && currentAssociation">
-				<GeneralInformation :association="currentAssociation" />
+				<template v-if="userStore.user?.scopes === 'global_access_scope'">
+					<DataGridComponent 
+						:store="associationsStore" 
+						:model-value="associationsTransformed" 
+						:title="getCapitalizedText(t('navigation.all-associations'))"
+						:description="getCapitalizedText(t('pages.associations.associations'))"
+						:columns="[
+						{ label: getCapitalizedText(t('common.name')), key: 'name' },
+						{ label: getCapitalizedText(t('pages.associations.siret')), key: 'siret' },
+						{ label: getCapitalizedText(t('pages.animals.status')), key: 'status' },
+					]" @edit="editItem" />
+				</template>
+				<template v-else>
+					<GeneralInformation :association="currentAssociation" />
+				</template>
 			</template>
 			<template v-if="currentTab === 1 && currentAssociation">
-				<DataGridComponent :store="associationsStore" :model-value="associationsTransformed" :columns="[
-					{ label: getCapitalizedText(t('common.name')), key: 'name' },
-					{ label: getCapitalizedText(t('pages.associations.siret')), key: 'siret' },
-					{ label: getCapitalizedText(t('pages.animals.status')), key: 'status' },
-				]" @edit="editItem" />
 			</template>
 		</div>
 	</div>
