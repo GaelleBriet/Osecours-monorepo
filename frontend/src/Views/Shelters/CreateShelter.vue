e12e87551c367137cece97b07a6007a8bd528cf8
uriostegui-sam <samyy.uv@gmail.com>
2024-04-16 11:51:01 +0200

    feat: creation and modification of Shelters

---
+++ frontend/src/Views/Shelters/CreateShelter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+	import SheltersFormComponent from '@/Components/SheltersFormComponent.vue';
+</script>
+
+<template>
+	<SheltersFormComponent :isCreateMode="true" />
+</template>
+
+<style scoped lang="postcss"></style>
