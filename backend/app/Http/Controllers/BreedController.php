b9bdd94cec8893568179d589943181e4ed10f384
MatteoDv51 <matteo.berlanas@cogenio.com>
2024-04-16 11:50:39 +0200

    add filter on breeds and colors all method

--- backend/app/Http/Controllers/BreedController.php
+++ backend/app/Http/Controllers/BreedController.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Breed;
+use App\Models\Specie;
+use Exception;
 use Illuminate\Http\Request;
 
 class BreedController extends Controller
 {
     //
-    public function getAll()
+    public function getAll(Request $request)
     {
-        return Breed::all();
+        $query = Breed::with(['specie']);
+        $breeds = Breed::all();      
+
+        if ($request->has('type')) {    
+            $type = ucfirst($request->type);
+            $specieExist = Specie::where('name', $type)->first();      
+            if(!$specieExist){
+                throw new Exception('Specie #'. $request->type . " not found",404);
+            }
+            $query->whereHas('specie', function ($query) use ($type) {
+                $query->where('name', $type); 
+            });
+
+            $breeds = $query->get();
+        }              
+
+        return $breeds;
     }
 
     public function show(Breed $breed)
--- backend/app/Http/Controllers/ColorController.php
+++ backend/app/Http/Controllers/ColorController.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Color;
+use App\Models\Specie;
+use Exception;
 use Illuminate\Http\Request;
 
 class ColorController extends Controller
 {
     //
-    public function getAll()
+    public function getAll(Request $request)
     {
-        return Color::all();
+        $query = Color::with(['specie']);
+        $colors = Color::all();      
+
+        if ($request->has('type')) {    
+            $type = ucfirst($request->type);
+            $specieExist = Specie::where('name', $type)->first();      
+            if(!$specieExist){
+                throw new Exception('Specie #'. $request->type . " not found",404);
+            }
+            $query->whereHas('specie', function ($query) use ($type) {
+                $query->where('name', $type); 
+            });
+
+            $colors = $query->get();
+        }              
+
+        return $colors;
     }
 
     public function show(Color $color)
