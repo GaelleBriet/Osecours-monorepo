9d5a0aab394623eca5e7f73ef6da939ce90c4352
GaelleBriet <gaelle.briet@protonmail.com>
2024-04-19 18:38:44 +0200

    wip : vaccines

--- frontend/src/Views/Animals/Health/VaccinesForm.vue
+++ frontend/src/Views/Animals/Health/VaccinesForm.vue
@@ -1,14 +1,22 @@
 <script lang="ts" setup>
 	import FormDate from '@/Components/Forms/FormDate.vue';
 	import FormSelect from '@/Components/Forms/FormSelect.vue';
+	import { generateOptionsWithDefault } from '@/Services/Helpers/Enums.ts';
+	import { Vaccines } from '@/Enums/Vaccines.ts';
+
+	const vaccinesOptions = generateOptionsWithDefault(
+		Vaccines,
+		'enums.animalVaccines',
+		'Choisir un vaccin',
+	);
 </script>
 <template>
 	<div class="">
 		<FormSelect
 			id="vaccine-type"
-			name="vaccine"
-			options=""
-			label="Ajouter un vaccin"
+			:name="'vaccine'"
+			:options="vaccinesOptions"
+			:label="'Ajouter un vaccin'"
 		/>
 		<FormDate
 			id="vaccine-date"
