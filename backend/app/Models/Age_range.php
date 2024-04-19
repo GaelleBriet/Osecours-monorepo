0ac74764b59faa3ceefa2108ae2a56a1d1c4ee9e
renesenses <bertrand.clech@oclock.school>
2024-04-19 15:47:10 +0200

    feat(dev): Add city address and association swagger annotations

--- backend/app/Models/Age_range.php
+++ backend/app/Models/Age_range.php
@@ -5,6 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Schema(
+ *     title="Age Range",
+ *     description="Age Range model",
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="ID of the age range",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Name of the age range",
+ *         example="Adult"
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         type="string",
+ *         description="Description of the age range",
+ *         example="Between 3 to 7 years old"
+ *     ),
+ * )
+ */
 
 class Age_range extends Model
 {
