0f80f95aae319d091b1eb7aedbc8b6d2399c0f59
GaelleBriet <gaelle.briet@protonmail.com>
2024-05-01 19:40:27 +0200

    linter

--- frontend/src/Components/TabsComponent.vue
+++ frontend/src/Components/TabsComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { computed, onMounted, ref, watch } from 'vue';
+	import { onMounted, ref } from 'vue';
 
 	// defineProps({
 	// 	tabs: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
