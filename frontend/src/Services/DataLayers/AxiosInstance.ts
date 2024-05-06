d9e473da57b4415c8eb55437968430454f167799
GaelleBriet <gaelle.briet@protonmail.com>
2024-05-06 13:32:22 +0200

    fix error at first login token not in localStorage

--- frontend/src/Services/DataLayers/AxiosInstance.ts
+++ frontend/src/Services/DataLayers/AxiosInstance.ts
@@ -4,16 +4,25 @@ const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Récupérer le token du stockage local
-const token = localStorage.getItem('token');
-// Formater le token pour supprimer les guillemets
-const formattedToken = token ? token.replace(/^"|"$/g, '') : '';
-// Si un token est disponible, l'inclure dans les en-têtes de toutes les requêtes
-if (formattedToken) {
-	axiosInstance.defaults.headers.common['Authorization'] =
-		`Bearer ${formattedToken}`;
-}
-console.log(formattedToken);
-// axiosInstance.defaults.headers.post['Content-Type'] = 'application/json';
+axiosInstance.interceptors.request.use(
+	(config) => {
+		// Récupérer le token du stockage local
+		const token = localStorage.getItem('token');
+
+		// Formater le token pour supprimer les guillemets
+		const formattedToken = token ? token.replace(/^"|"$/g, '') : '';
+
+		// Si un token est disponible, l'inclure dans les en-têtes de toutes les requêtes
+		if (formattedToken) {
+			config.headers['Authorization'] = `Bearer ${formattedToken}`;
+		}
+
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
+);
 axiosInstance.defaults.headers['Content-Type'] = 'application/json';
+
 export default axiosInstance;
