08fa65a37808a1d0191aa8e083cffb45310b4bc0
GaelleBriet <gaelle.briet@protonmail.com>
2024-05-21 15:45:22 +0200

    Update Dockerfile and add start-apache.sh script

--- start-apache.sh
+++ start-apache.sh
@@ -2,8 +2,8 @@
 
 # Remplacer le port par la variable d'environnement $PORT définie par Heroku
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
-sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/apache2.conf
-sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:${PORT:-80}>/g" /etc/apache2/sites-available/000-default.conf
+#sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/apache2.conf
+#sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:${PORT:-80}>/g" /etc/apache2/sites-available/000-default.conf
 
 # Démarrer Apache
-apache2-foreground "$@"
+apache2-foreground
