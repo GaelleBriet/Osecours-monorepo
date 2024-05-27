3b26e20a698829476d5e32f53cb3e1317d363a6d
GaelleBriet <gaelle.briet@protonmail.com>
2024-05-27 08:50:20 +0200

    hotfix

--- frontend/src/Services/DataLayers/Document.ts
+++ frontend/src/Services/DataLayers/Document.ts
@@ -5,8 +5,19 @@ import { RouteParamValue } from 'vue-router';
 import { errorResponse } from '@/Services/Requests/RequestsResponses.ts';
 import axiosInstance from '@/Services/DataLayers/AxiosInstance.ts';
 
-const currentAssociation = JSON.parse(localStorage.getItem('user'));
-const associationId = currentAssociation.associations[0].id;
+let currentAssociation;
+const userItem = localStorage.getItem('user');
+if (userItem) {
+	currentAssociation = JSON.parse(userItem);
+}
+let associationId: any;
+if (
+	currentAssociation &&
+	currentAssociation.associations &&
+	currentAssociation.associations[0]
+) {
+	associationId = currentAssociation.associations[0].id;
+}
 
 export const getDocument = async (
 	id: string | RouteParamValue[],
