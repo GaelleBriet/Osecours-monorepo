ddffe8b40b0abf518579cb37ffca0e908b3b3fd6
GaelleBriet <gaelle.briet@protonmail.com>
2024-05-30 23:13:45 +0200

    fix some warnings (password input)

--- frontend/src/Components/Forms/FormPassword.vue
+++ frontend/src/Components/Forms/FormPassword.vue
@@ -21,6 +21,7 @@
 		validationMessages?: object;
 		classes?: object;
 		showConfirm?: boolean;
+    placeholder?: string;
 	}>();
 
 	const emit = defineEmits<{
@@ -92,6 +93,7 @@
 		:validation-messages="validationMessages"
 		:validation-visibility="validationVisibility"
 		:disabled="disabled"
+    :placeholder="placeholder"
 		suffix-icon="eyeClosed"
 		@suffix-icon-click="handleIconClick"
 		suffix-icon-class="hover:text-osecours-beige-dark"
