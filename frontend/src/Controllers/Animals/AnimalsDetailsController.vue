0213c83be6d62b277a88e8d539d398e5c0df492c
uriostegui-sam <samyy.uv@gmail.com>
2024-06-23 17:17:28 +0200

    fix: space below 'retourn' button

--- frontend/src/Controllers/Animals/AnimalsDetailsController.vue
+++ frontend/src/Controllers/Animals/AnimalsDetailsController.vue
@@ -33,7 +33,7 @@
 
 <template>
 	<div class="container">
-		<div class="flex flex-row justify-between">
+		<div class="flex flex-row justify-between mb-2">
 			<div class="text-2xl mb-1">
 				{{ getCapitalizedText(t('pages.animals.card')) }}:
 				{{ currentAnimal?.name }}
