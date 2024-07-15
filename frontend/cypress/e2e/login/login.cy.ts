d66378ce37132f3466f6fab35223ae5426dd6958
GaelleBriet <gaelle.briet@protonmail.com>
2024-07-15 17:48:54 +0200

    e2e test :  login.cy.ts (Login refused with wrong credentials)

--- frontend/cypress/e2e/login/login.cy.ts
+++ frontend/cypress/e2e/login/login.cy.ts
@@ -14,4 +14,13 @@ describe('Authentication tests', () => {
 		cy.get('#options-menu-item-2').should('exist').click();
 		cy.url().should('include', '/login');
 	});
+	it('Login refused with wrong credentials', () => {
+		cy.visit('/login');
+		cy.get('h2').should('have.text', 'Connectez-vous pour accéder à votre compte.');
+		cy.get('form').should('exist');
+		cy.get('#email').should('exist').type('email@test.fr');
+		cy.get('#password').should('exist').type('Azerty1*');
+		cy.get('button[type="submit"]').should('exist').click();
+		cy.get('#alert-component').should('exist').contains('Identifiants incorrects. Veuillez réessayer');
+	});
 });
