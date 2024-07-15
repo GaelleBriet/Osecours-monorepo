6a626213d1f902a74b9d13047aea48364ed3bf59
GaelleBriet <gaelle.briet@protonmail.com>
2024-07-15 14:32:06 +0200

    update e2e login test

--- frontend/cypress/e2e/login/login.cy.ts
+++ frontend/cypress/e2e/login/login.cy.ts
@@ -1,28 +1,27 @@
-describe('LoginPage test', () => {
+/// <reference types="cypress" />
+describe('LoginPage test, authenticate successfully', () => {
 	beforeEach(() => {
 		cy.visit('/login');
 	});
-	it('Check if the login page is loaded', () => {
+	it('Fill the form and submit, should redirect to home page', () => {
 		cy.get('h2').should(
 			'have.text',
 			'Connectez-vous pour accéder à votre compte.',
 		);
-	});
-	it('Check if login form and inputs are is loaded', () => {
 		cy.get('form').should('exist');
+
 		cy.get('#email').should('exist');
 		cy.get('#password').should('exist');
-	});
-	it('Check if submit button is loaded', () => {
-		cy.get('button[type="submit"]').should('exist');
-	});
-	it('Fill the form and submit, should redirect to home page', () => {
 		cy.fixture('user').then((user) => {
 			cy.get('#email').type(user.email);
 			cy.get('#password').type(user.password);
 		});
-		cy.get('button[type="submit"]').click();
-		cy.get('#association').select('1');
+
+		cy.get('button[type="submit"]').should('exist').click();
+
+		cy.get('#association').should('exist')
+			.select('Fondation Brigitte Bardot')
+			.should('have.value', '1');
 		cy.url().should('include', '/');
 	});
 });
