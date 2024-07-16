62d1e5635f7ddf1a23b5b8cf81f9f84c57889641
GaelleBriet <gaelle.briet@protonmail.com>
2024-07-16 18:51:36 +0200

    removed mockery

--- backend/tests/Unit/Services/AuthServiceTest.php
+++ backend/tests/Unit/Services/AuthServiceTest.php
@@ -9,13 +9,15 @@ use App\Http\Services\AuthService;
 use App\Http\Services\ErrorService;
 use App\Enum\UserStatus;
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
-use Mockery;
 
 class AuthServiceTest extends TestCase
 {
+    use DatabaseTransactions;
+
     protected $authController;
     protected $errorServiceMock;
     protected $authServiceMock;
@@ -27,13 +29,8 @@ class AuthServiceTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->errorServiceMock = Mockery::mock(ErrorService::class);
-        $this->authServiceMock = Mockery::mock('overload:' . \App\Http\Services\AuthService::class);
-
-        $this->authController = new AuthController($this->errorServiceMock);
-        $this->authService = new AuthService();
         $this->errorService = new ErrorService();
-        $this->auth = Auth::class;
+        $this->authController = new AuthController($this->errorService);
     }
 
     public function test_login__user()
@@ -42,27 +39,23 @@ class AuthServiceTest extends TestCase
         $password = 'P@ssword_1';
         $user = User::first('email', $email);
 
-        // créer manuellement une requête et la valider en dehors du service container
-        // en env. de test, le laravel service container ne resoud pas les dépendances automatiquement
-        // il faut donc le faire manuellement
-        // le redirector n'est pas défini dans l'objet AuthRequest lors de l'éxécution des tests
-        // il génère des URL pour les redirections
         $request = new AuthRequest();
         $request->setContainer(app());
         $request->setRedirector(app('redirect'));
         $request->initialize(['email' => $email, 'password' => $password]);
 
-        // Simuler le comportement attendu de AuthService
-        $this->authServiceMock->shouldReceive('connectUser')
-            ->once()
-            ->with(['email' => $email, 'password' => $password])
-            ->andReturn([
-                'status' => UserStatus::CONNECTED->value,
-                'associations' => [
-                    ['id' => 1, 'name' => 'Association 1'],
-                    ['id' => 2, 'name' => 'Association 2'],
-                ],
-            ]);
+        $authService = new AuthService();
+        $result = $authService->connectUser(['email' => $email, 'password' => $password]);
+
+        $result['associations'] = $result['associations']->toArray();
+        $this->assertEquals([
+            'status' => UserStatus::CONNECTED->value,
+            'associations' => [
+                ['id' => 1, 'name' => 'Fondation Brigitte Bardot'],
+                ['id' => 31, 'name' => 'Fondation 30 Millions d\'Amis'],
+                ['id' => 61, 'name' => 'Société Protectrice des Animaux'],
+            ],
+        ], $result);
 
         $request->validateResolved();
 
@@ -73,7 +66,7 @@ class AuthServiceTest extends TestCase
         $responseData = json_decode($response->getContent(), true);
         $this->assertArrayHasKey('data', $responseData);
         $this->assertEquals(UserStatus::CONNECTED->value, $responseData['data']['status']);
-        $this->assertCount(2, $responseData['data']['associations']);
+        $this->assertCount(3, $responseData['data']['associations']);
     }
 
     public function test_login__user_with_invalid_credentials()
@@ -86,17 +79,14 @@ class AuthServiceTest extends TestCase
         $request->setRedirector(app('redirect'));
         $request->initialize(['email' => $email, 'password' => $password]);
 
-        // Simuler un échec de connexion
-        $this->authServiceMock->shouldReceive('connectUser')
-            ->once()
-            ->with(['email' => $email, 'password' => $password])
-            ->andThrow(UnauthorizedException::class);
+        $authService = new AuthService();
 
-        // Définir une attente pour la méthode handle() de ErrorService
-        $this->errorServiceMock->shouldReceive('handle')
-            ->once()
-            ->with(Mockery::type(UnauthorizedException::class))
-            ->andReturn(new JsonResponse(['error' => 'Unauthorized'], 401));
+        try {
+            $authService->connectUser(['email' => $email, 'password' => $password]);
+            $this->fail('Expected UnauthorizedException not thrown');
+        } catch (UnauthorizedException $e) {
+            $this->assertEquals('Invalid credentials', $e->getMessage());
+        }
 
         $request->validateResolved();
 
@@ -108,12 +98,6 @@ class AuthServiceTest extends TestCase
 
         $responseData = json_decode($response->getContent(), true);
         $this->assertArrayHasKey('error', $responseData);
-        $this->assertEquals('Unauthorized', $responseData['error']);
-    }
-
-    public function tearDown(): void
-    {
-        Mockery::close();
-        parent::tearDown();
+        $this->assertEquals('Invalid credentials', $responseData['error']);
     }
 }
